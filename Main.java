b4e3703ddd67f8af7735732460db4478a811cb06
Liam <w0494713@nscc.ca>
2025-01-13 09:35:27 -0400

    trying to re add main java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+    Book walnut = new Book();
+    walnut.setPineapple ("The Very Hungry Caterpillar");
+    System.out.println(walnut.getPineapple());
+
+    }
+
+}

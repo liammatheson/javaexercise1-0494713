d6a1a639ee5c99e9e40b072bc43e5f6fe67cc626
Liam <w0494713@nscc.ca>
2025-01-13 09:25:12 -0400

    exercise 2 v1

---
+++ Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    private String Pineapple;
+
+    public String getPineapple() {
+        return Pineapple;
+    }
+
+    public void setPineapple(String Bookname) {
+        this.Pineapple = Bookname;
+    }
+}
